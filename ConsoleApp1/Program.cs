c311887cb6894594befb4807f66c53d7705b40d2
azubalispovilas <azubalispovilas@gmail.com>
2024-06-03 13:56:51 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PirmaUzduotis_Pagrinidinis_AutomobiliuNuoma
+{
+        // Sukurkite automobilių nuomos sistemą.
+
+        // Jums reikės sukurti pagrindinę klasę Automobilis,
+        // kuri turės šiuos atributus: marke, modelis, metai ir kaina už dieną.
+        // Sukurkite reikiamus getterius ir setterius,
+        // taip pat konstruktorių, leidžiantį sukurti automobilio objektą su nurodytais atributais.
+
+        // Toliau sukurti dvi subklases, NaftosKuroAutomobilis ir ElektrinisAutomobilis.
+        // Kiekviena subklasė turi papildomą atributą, pavyzdžiui,
+        // degalų suvartojimas 100 km atstumu arba baterijos krovimo laikas.
+        // Taip pat, kiekviena subklasė turi turėti savo konstruktorių, kuris prideda šiuos papildomus atributus,
+        // ir tinkamus getterius ir setterius.
+
+        // Galų gale, parašykite klasę Nuoma, kurioje yra sąrašas su turimais automobiliais.
+        // Sukurti metodus, kurie leistų pridėti naujus automobilius į sąrašą,
+        // pasiimti automobilį iš sąrašo ir apskaičiuoti nuomos kainą už nurodytą dienų skaičių.
+        // Pridėti viso autoparko atspausdinimą. Susikūrus klasę autoparkas
+        // prisidėti sąrašą automobilių.
+        class UI
+        {
+                public static void Main(string[] args)
+                {
+
+                }
+        }
+}
